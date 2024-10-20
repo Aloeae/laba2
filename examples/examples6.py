a185397e00d068ebc8d60247dc860216d3a41a67
Alyona Efremova <142985072+Aloeae@users.noreply.github.com>
2024-10-20 23:57:42 +0500

    Update examples6.py

--- examples/examples6.py
+++ examples/examples6.py
@@ -3,3 +3,8 @@ def cat(name, color, age):
   
 my_cat = cat('Alise', 'Grey', 9)
 print(my_cat)
+
+
+
+==================== RESTART: C:/Users/tvoym/Downloads/ex.py ===================
+{'name': 'Alise', 'color': 'Grey', 'age': 9}
