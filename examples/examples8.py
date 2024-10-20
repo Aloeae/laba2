84d8d9a2b0151da6d5f296b70675fea82d2b7ace
Alyona Efremova <142985072+Aloeae@users.noreply.github.com>
2024-10-20 23:59:10 +0500

    Update examples8.py

--- examples/examples8.py
+++ examples/examples8.py
@@ -3,3 +3,9 @@ def example_args(*args):
 
 example_args()
 example_args(1, 2, 4, 'argument')
+
+
+
+==================== RESTART: C:/Users/tvoym/Downloads/ex.py ===================
+Positional argument tuple: ()
+Positional argument tuple: (1, 2, 4, 'argument')
