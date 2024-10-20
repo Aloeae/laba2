8ac2401e181a3281e9c09efa8cedce9af19c31a0
Alyona Efremova <142985072+Aloeae@users.noreply.github.com>
2024-10-20 23:51:35 +0500

    Update examples2.py

--- examples/examples2.py
+++ examples/examples2.py
@@ -2,3 +2,7 @@ def example():
   print("Hello, World! (c) Kazantsev")
   
 example()
+
+
+PS C:\Users\tvoym\test\test> python -u "c:\Users\tvoym\test\test\Untitled-1.py"
+Hello, World! (c) Kazantsev
