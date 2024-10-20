9e12570510efda53678597996ec2ac12f9f12f00
Alyona Efremova <142985072+Aloeae@users.noreply.github.com>
2024-10-21 00:07:51 +0500

    Update and rename examples_14.py to examples_13.py

--- examples/examples_13.py
+++ examples/examples_13.py
@@ -5,3 +5,10 @@ def book_list(books, func):
 books = ['System Design','Python и DevOps','Git. Практическое руководство']
 
 book_list(books, lambda book: book.upper() + ' - прочитано')
+
+
+
+==================== RESTART: C:/Users/tvoym/Downloads/ex.py ===================
+SYSTEM DESIGN - прочитано
+PYTHON И DEVOPS - прочитано
+GIT. ПРАКТИЧЕСКОЕ РУКОВОДСТВО - прочитано
